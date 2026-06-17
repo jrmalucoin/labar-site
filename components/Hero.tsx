d05ab89eafddeee5ae640e7ae9b612980642463e
jrmalucoin <jrmalucoin@gmail.comm>
2026-06-16 23:48:51 -0300

    Descrição

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,7 @@ export default function Hero() {
             text-blue-400
           "
           >
-            Inteligência Artificial • WhatsApp do JR • Automação
+            Inteligência Artificial • WhatsApp • Automação
           </span>
 
           <h1
