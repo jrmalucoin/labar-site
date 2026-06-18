f0c75ba16c998ec9aebd01e63e0eb28168596a17
jrmalucoin <jrmalucoin@gmail.comm>
2026-06-18 00:18:08 -0300

    Alterando modo do site

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import NovoSite from "./novo-site/page";
 import Manutencao from "./manutencao/page";
 
+
+// se igual a true site em manutenção, se igual a false site em produção
+
+const SITE_EM_MANUTENCAO = false;
+
 export default function Home() {
-  return <Manutencao />;
+  return SITE_EM_MANUTENCAO
+    ? <Manutencao />
+    : <NovoSite />;
 }
