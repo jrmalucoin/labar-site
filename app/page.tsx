90c5065212f7c6f65e98e54e366467da07fd5ef4
jrmalucoin <jrmalucoin@gmail.comm>
2026-06-18 19:21:50 -0300

    teste visual

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 import NovoSite from "./novo-site/page";
 import Manutencao from "./manutencao/page";
 
+export default function Home() {
+  return (
+    <div style={{fontSize:"50px"}}>
+      TESTE 18/06/2026 19:30
+    </div>
+  );
+}
+
+
+/*
 
 // se igual a true site em manutenção, se igual a false site em produção
 
@@ -11,3 +21,4 @@ export default function Home() {
     ? <Manutencao />
     : <NovoSite />;
 }
+*/
