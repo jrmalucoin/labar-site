307b99c735e437042ea3067c7699938223964e92
jrmalucoin <jrmalucoin@gmail.comm>
2026-06-18 01:15:05 -0300

    Site em manutenção

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,13 @@
 import NovoSite from "./novo-site/page";
 import Manutencao from "./manutencao/page";
 
-/*
+
 // se igual a true site em manutenção, se igual a false site em produção
 
-const SITE_EM_MANUTENCAO = false;
+const SITE_EM_MANUTENCAO = true;
 
 export default function Home() {
   return SITE_EM_MANUTENCAO
     ? <Manutencao />
     : <NovoSite />;
-}
-
-
-*/
-
-
-
-
-
-export default function Home() {
-  return (
-    <h1 style={{fontSize:"60px"}}>
-      TESTE JR 999
-    </h1>
-  );
 }
