197947df2aee0d243f92a870c3dd42afeaaefa84
jrmalucoin <jrmalucoin@gmail.comm>
2026-06-18 20:10:59 -0300

    forcando rebuild

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,9 @@ git add .
 git commit -m "$1"
 git push origin main
 
-curl "http://168.231.65.61:3000/api/deploy/bee6f3a5f25dcd47a5e8c07ae85eb1a07d9481fdb0720aec"
+echo "Iniciando deploy..."
+
+curl -s "http://168.231.65.61:3000/api/deploy/bee6f3a5f25dcd47a5e8c07ae85eb1a07d9481fdb0720aec"
+
+echo ""
+echo "Deploy enviado para o Easypanel."
