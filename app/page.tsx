8bc5307330f37a2f8b2e7a86394c4b567cc5cc9e
jrmalucoin <jrmalucoin@gmail.comm>
2026-06-18 20:45:24 -0300

    Voltar para manutencao

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Manutencao from "./manutencao/page";
 
 // se igual a true site em manutenção, se igual a false site em produção
 
-const SITE_EM_MANUTENCAO = false;
+const SITE_EM_MANUTENCAO = true;
 
 export default function Home() {
   return SITE_EM_MANUTENCAO
