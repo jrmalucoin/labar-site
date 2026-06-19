f46187fbc3d0a38b113006db94ad2ec201b2fa2c
jrmalucoin <jrmalucoin@gmail.comm>
2026-06-19 14:13:11 -0300

    Teste webhook dev

--- arquivos-atualizacao/deploy-prod.sh
+++ arquivos-atualizacao/deploy-prod.sh
@@ -4,4 +4,4 @@ git add .
 git commit -m "$1"
 git push origin main
 
-curl "http://168.231.65.61:3000/api/deploy/cc3f05447b08dcc"
+curl "http://168.231.65.61:3000/api/deploy/bee6f3a5f25dcd47a5e8c07ae85eb1a07d9481fdb0720aec"
