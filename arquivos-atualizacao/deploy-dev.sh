7191d4ad13deaec27a53296c0365ed627f36699c
jrmalucoin <jrmalucoin@gmail.comm>
2026-06-19 14:00:36 -0300

    Novo banner home

--- arquivos-atualizacao/deploy-dev.sh
+++ arquivos-atualizacao/deploy-dev.sh
@@ -4,4 +4,4 @@ git add .
 git commit -m "$1"
 git push origin dev
 
-curl "http://168.231.65.61:3000/api/deploy/7d9481fdb0720aec"
+curl "http://168.231.65.61:3000/api/deploy/http://168.231.65.61:3000/api/deploy/e82f65aa95a505b89121c646d8707573fcc3f05447b08dcc"
