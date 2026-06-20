8e6f54c11863b5771c5e08c80f807530e1d80587
jrmalucoin <jrmalucoin@gmail.comm>
2026-06-20 00:45:32 -0300

    Lê variável de manutenção em runtime

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import NovoSite from "./novo-site/page";
 import Manutencao from "./manutencao/page";
 
-const SITE_EM_MANUTENCAO =
-  process.env.SITE_EM_MANUTENCAO === "true";
+export const dynamic = "force-dynamic";
 
-export default function Home() {
+export default async function Home() {
+  const SITE_EM_MANUTENCAO =
+    process.env.SITE_EM_MANUTENCAO === "true";
+
   return SITE_EM_MANUTENCAO
     ? <Manutencao />
     : <NovoSite />;
