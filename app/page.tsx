46935a24b0fa6917a37abd406a22fb6953616ee8
jrmalucoin <jrmalucoin@gmail.comm>
2026-06-20 00:09:07 -0300

    Controle de manutenção por variável de ambiente

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import NovoSite from "./novo-site/page";
 import Manutencao from "./manutencao/page";
 
-// se igual a true site em manutenção, se igual a false site em produção
-
-const SITE_EM_MANUTENCAO = false;
+const SITE_EM_MANUTENCAO =
+  process.env.SITE_EM_MANUTENCAO === "true";
 
 export default function Home() {
   return SITE_EM_MANUTENCAO
